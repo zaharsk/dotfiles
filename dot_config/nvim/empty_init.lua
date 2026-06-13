6e6abbe0e5684e3023cb66743a7e43046e616a4a
Zakhar Skorokhodov <zaharsk@gmail.com>
2026-06-13 11:15:35 +0200

    feat: Addde NeoVIM

---
+++ dot_config/nvim/empty_init.lua
@@ -0,0 +1,11 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.textwidth = 80
+
+vim.diagnostic.config({ virtual_text = true })
